0ec08943473dcacaa812cd14a8408970b4647eeb
EliottmacR <47741786+EliottmacR@users.noreply.github.com>
2019-06-20 13:06:11 +0200

    Revert "Message for lvl up"
    This reverts commit a5aa6c1adda4f216a0461ec6acdc0c48ba58b317.

--- waves.lua
+++ waves.lua
@@ -13,18 +13,14 @@ function init_waves()
   waves = {
     --[enemy_type] = remaining
     { -- 1
-      [1] = 1,
+      [1] = 0,
       [2] = 0
     }
   }
-  -- for i = 1, 10 do 
-    -- waves[i] = { 5 + i * (2 - ceil(i/5)), i * 2}
+  for i = 1, 10 do 
+    waves[i] = { 5 + i * (2 - ceil(i/5)), i * 2}
   
-  -- end
-  -- for i = 1, 10 do 
-    -- waves[i] = { 5 + i * (2 - ceil(i/5)), i * 2}
-  
-  -- end
+  end
   
   spawning_enemies = false
   current_wave = 0
