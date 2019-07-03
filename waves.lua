d0710b970eb4c491725edc55fac6e9182aa5b805
EliottmacR <47741786+EliottmacR@users.noreply.github.com>
2019-07-03 21:05:17 +0200

    Update waves.lua

--- waves.lua
+++ waves.lua
@@ -101,7 +101,7 @@ function random_enemy_type(current_wave)
   if not current_wave then
     local choosen_id = 1 + irnd(100)
     
-    if choosen_id < 45 then return 1 end
+    if choosen_id < 40 then return 1 end
     
     if choosen_id < 60 then return 2 end
     
